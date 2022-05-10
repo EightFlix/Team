27818981b12fe2354b498823446ed03da5f2327b
TeamQuto <103844261+TeamQuto@users.noreply.github.com>
2022-05-10 22:21:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -750,22 +750,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton(' Help', callback_data='help'),
             InlineKeyboardButton('About', callback_data='about')
         ]]
-        reply1 = await query.message.reply_text(
-            text="â–¢â–¢â–¢"
-        )
-        await asyncio.sleep(0.5)
-        reply2 = await reply1.edit_text(
-            text="â–£â–¢â–¢"
-        )
-        await asyncio.sleep(0.5)
-        reply3 = await reply2.edit_text(
-            text="â–£â–£â–¢"
-        )
-        await asyncio.sleep(0.5)
-        reply4 = await reply3.edit_text(
-            text="â–£â–£â–£"
-        )
-        await reply4.delete()
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
@@ -900,22 +884,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('🏠 𝙷𝙾𝙼𝙴 🏠', callback_data='start'),
             InlineKeyboardButton('🔐 𝙲𝙻𝙾𝚂𝙴 🔐', callback_data='close_data')
         ]]
-         reply1 = await query.message.reply_text(
-            text="â–¢â–¢â–¢"
-        )
-        await asyncio.sleep(0.5)
-        reply2 = await reply1.edit_text(
-            text="â–£â–¢â–¢"
-        )
-        await asyncio.sleep(0.5)
-        reply3 = await reply2.edit_text(
-            text="â–£â–£â–¢"
-        )
-        await asyncio.sleep(0.5)
-        reply4 = await reply3.edit_text(
-            text="â–£â–£â–£"
-        )
-        await reply4.delete()
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.ABOUT_TXT.format(temp.B_NAME),

ac8da7b5b7430f626d23c884a69272b20568059c
TeamQuto <103844261+TeamQuto@users.noreply.github.com>
2022-05-10 21:56:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -750,37 +750,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton(' Help', callback_data='help'),
             InlineKeyboardButton('About', callback_data='about')
         ]]
-          reply1 = await query.message.reply_text(
-
-            text="â–¢â–¢â–¢"
-
-        )
-
-        await asyncio.sleep(0.5)
-
-        reply2 = await reply1.edit_text(
-
-            text="â–£â–¢â–¢"
-
-        )
-
-        await asyncio.sleep(0.5)
-
-        reply3 = await reply2.edit_text(
-
-            text="â–£â–£â–¢"
-
-        )
-
-        await asyncio.sleep(0.5)
-
-        reply4 = await reply3.edit_text(
-
-            text="â–£â–£â–£"
-
-        )
-
-        await reply4.delete()
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),

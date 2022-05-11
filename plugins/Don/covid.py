f0347a0cfbf1c8dd5dcd3a1dffce779ef86a3f35
TeamSuperX <101799200+TeamSuperX@users.noreply.github.com>
2022-05-11 16:26:26 +0530

    Update covid.py

--- plugins/Don/covid.py
+++ plugins/Don/covid.py
@@ -12,7 +12,7 @@ BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton("𝙲𝙻𝙾𝚂𝙴", ca
 async def reply_info(client, message):
     query = message.text.split(None, 1)[1]
     await message.reply_photo(
-        photo="https://telegra.ph/file/1b837a8df2670b0097aaf.jpg",
+        photo="https://telegra.ph/file/f8e729791a2333a5088f0.jpg",
         caption=covid_info(query),
         quote=True
     )

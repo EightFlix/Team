7e7ad2eeffa64cf0a14211305ccd621136c20070
Vishal Mehta <103929700+EightFlix@users.noreply.github.com>
2022-05-12 11:20:45 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -748,7 +748,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('Add Me Your Group', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[      
             InlineKeyboardButton(' Help', callback_data='help'),
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat='')
+            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
             InlineKeyboardButton('About', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)

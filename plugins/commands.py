e5bbaf9bc70ba0814501bb2752edc6faab9aa25c
Vishal Mehta <103929700+EightFlix@users.noreply.github.com>
2022-05-12 11:13:59 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -39,10 +39,10 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton(' Add Me Your Group', url=f'http://t.me/{temp.U_NAME}?startgroup=true'),
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat='')
+            InlineKeyboardButton(' Add Me Your Group', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[      
             InlineKeyboardButton('Help', callback_data='help'),
+            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat='')
             InlineKeyboardButton('About', callback_data='about')
         ]]         
         reply_markup = InlineKeyboardMarkup(buttons)        
@@ -78,10 +78,10 @@ async def start(client, message):
         return
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton(' Add Me Your Group', url=f'http://t.me/{temp.U_NAME}?startgroup=true'),
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat='')
+            InlineKeyboardButton(' Add Me Your Group', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[      
             InlineKeyboardButton('Help', callback_data='help'),
+            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat='')
             InlineKeyboardButton('About', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
